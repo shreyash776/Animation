3b29988362973e37b716b15b14d9b086ab45472a
shreyash <shreyashchaurasia77@gmail.com>
2023-10-02 11:59:07 +0530

    fixing errors in moving div

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
- 
-  let pointer = document.querySelector(".moving-cursor");
-        window.addEventListener('mousemove',cursor);
- function cursor(event){
-    pointer.style.top=event.pageY+"px";
-    pointer.style.left=event.pageX +"px";
+let pointer = document.querySelector(".moving-cursor");
+window.addEventListener("mousemove", cursor);
+function cursor(event) {
+  pointer.style.top = event.pageY + "px";
+  pointer.style.left = event.pageX + "px";
+}
 
-  }
+let nav_items = document.querySelectorAll(".nav-item");
 
-   let nav_items = document.querySelectorAll(".nav-item");
-  nav_items.forEach((element)=>{
-    element.addEventListener('mouseover',function(){
-        
-        // pointer.classList.remove("moving-cursor");
-        pointer.classList.add("cursor-grow");
-        
-       });
-      element.addEventListener('mouseleave',()=>{
-        element.classList.remove("cursor-grow");
-        element.classList.add("moving-cursor");
-      });
-  }
-
-    );
+nav_items.forEach((element) => {
+  element.addEventListener("mouseenter", function () {
+    pointer.classList.add("cursor-grow");
+  });
+  element.addEventListener("mouseleave", () => {
+    pointer.classList.remove("cursor-grow");
+  });
+});
